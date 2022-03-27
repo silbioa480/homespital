d4a24bfd3c62264266a60243e982f7919e2d4be4
silbioa480 <silbioa480@gmail.com>
2022-03-27 20:19:27 +0900

    project init

---
+++ src/main/java/mna/homespital/HomespitalApplication.java
@@ -0,0 +1,13 @@
+package mna.homespital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomespitalApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HomespitalApplication.class, args);
+  }
+
+}
---
+++ src/test/java/mna/homespital/HomespitalApplicationTests.java
@@ -0,0 +1,13 @@
+package mna.homespital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomespitalApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
