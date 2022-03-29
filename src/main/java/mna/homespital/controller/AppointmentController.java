df61a7d9efbc939577cb874b65e3dc5b3306ef05
ekjkkk153 <ekjkkk153@daum.net>
2022-03-29 19:09:09 +0900

    진료과 선택 jsp 및 컨트롤러 생성, MySQL 서버 SSH 터널링 구성

---
+++ src/main/java/mna/homespital/controller/AppointmentController.java
@@ -0,0 +1,16 @@
+package mna.homespital.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/appointment")
+public class AppointmentController {
+    @GetMapping("/writeChart")
+    public ModelAndView writeChart() {
+        ModelAndView mv = new ModelAndView("/user/writeMediChartForm");
+        return mv;
+    }
+}
