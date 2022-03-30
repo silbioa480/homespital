1c9729f7b0bd91d276fc8f119c6da8a8afdf611a
INYONGSIK <97017840+INYONGSIK@users.noreply.github.com>
2022-03-31 03:20:00 +0900

    용식:회원가입컨트롤러추가

--- src/main/java/mna/homespital/controller/SignUpController.java
+++ src/main/java/mna/homespital/controller/SignUpController.java
@@ -1,2 +1,20 @@
-package mna.homespital.controller;public class SignUpController {
+package mna.homespital.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import service.phoneCheckService;
+
+@Controller
+public class SignUpController {
+    @RequestMapping(value = "/phoneCheck", method = RequestMethod.GET)
+    @ResponseBody
+    public String sendSMS(@RequestParam("phone") String userPhoneNumber) { // 휴대폰 문자보내기
+        int randomNumber = (int) ((Math.random() * (9999 - 1000 + 1)) + 1000);//난수 생성
+        phoneCheckService phoneCheckService = new phoneCheckService();
+        phoneCheckService.certifiedPhoneNumber(userPhoneNumber, randomNumber);
+        return Integer.toString(randomNumber);
+    }
 }
