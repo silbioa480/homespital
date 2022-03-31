5ebde1b6c02730b40ee0da4616f657ff05d2f8d8
silbioa480 <silbioa480@gmail.com>
2022-03-31 10:00:13 +0900

    modfied DiagnosisDAO.java

--- src/main/java/mna/homespital/dao/DiagnosisDAO.java
+++ src/main/java/mna/homespital/dao/DiagnosisDAO.java
@@ -1,10 +1,11 @@
 package mna.homespital.dao;
 
+import mna.homespital.dto.Diagnosis;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
 @Mapper
 @Repository
 public interface DiagnosisDAO {
-    public void insertDiag(DiagnosisDTO diag) throws Exception;
+  public void insertDiag(Diagnosis diag) throws Exception;
 }
