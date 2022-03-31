ee9d593aeceeb98c509bfc3ca42ea78f80a15a2d
silbioa480 <silbioa480@gmail.com>
2022-03-31 09:56:22 +0900

    modified RootController.java

--- src/main/java/mna/homespital/controller/RootController.java
+++ src/main/java/mna/homespital/controller/RootController.java
@@ -9,42 +9,43 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("/")
 public class RootController {
 
-    public RootController() {
-    }
 
-    @GetMapping("/")
-    public ModelAndView main() {
-        return new ModelAndView("main/main");
-    }
+  public RootController() {
+  }
 
-    //로그인
-    @GetMapping("/login")
-    public ModelAndView login() {
-        return new ModelAndView("login");
-    }
+  @GetMapping("/")
+  public ModelAndView main() {
+    return new ModelAndView("main/main");
+  }
 
-    //회원가입
-    @GetMapping("/signUp")
-    public ModelAndView signUp() {
-        return new ModelAndView("signup");
-    }
+  //로그인
+  @GetMapping("/login")
+  public ModelAndView login() {
+    return new ModelAndView("login");
+  }
 
-    //비밀번호 찾기
-    @GetMapping("/pwsearch")
-    public ModelAndView pwsearch() {
-        return new ModelAndView("pwsearch");
-    }
+  //회원가입
+  @GetMapping("/signUp")
+  public ModelAndView signUp() {
+    return new ModelAndView("signup");
+  }
 
-    //의료진 찾기
-    @GetMapping("/doctorList")
-    public ModelAndView doctorList() {
-        return new ModelAndView("doctorList");
-    }
+  //비밀번호 찾기
+  @GetMapping("/pwsearch")
+  public ModelAndView pwsearch() {
+    return new ModelAndView("pwsearch");
+  }
 
-    //진료차트 쓰기
-    @GetMapping("/writeChart")
-    public ModelAndView writeChart() {
-        ModelAndView mv = new ModelAndView("/user/writeMediChartForm");
-        return mv;
-    }
+  //의료진 찾기
+  @GetMapping("/doctorList")
+  public ModelAndView doctorList() {
+    return new ModelAndView("doctorList");
+  }
+
+  //진료차트 쓰기
+  @GetMapping("/writeChart")
+  public ModelAndView writeChart() {
+    ModelAndView mv = new ModelAndView("/user/writeMediChartForm");
+    return mv;
+  }
 }
