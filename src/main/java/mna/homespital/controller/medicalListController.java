5fe2a6471680a3b0e06bee1e3534a31a7179b4d8
sintaeyoung <86948066+sintaeyoung@users.noreply.github.com>
2022-04-01 09:11:37 +0900

    taeyoung medicalcontroller

--- src/main/java/mna/homespital/controller/medicalListController.java
+++ src/main/java/mna/homespital/controller/medicalListController.java
@@ -2,17 +2,35 @@ package mna.homespital.controller;
 
 
 
+import mna.homespital.dto.allMedical;
+import mna.homespital.service.allMedicalListService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.List;
+
+
 @RestController
 public class medicalListController {
 
+    @Autowired
+    allMedicalListService allmedListService;
+
     @GetMapping("/list")
     public ModelAndView medicalList(){
+        ModelAndView mv=new ModelAndView();
+        try{
+            List<allMedical> amd=allmedListService.allMedList();
+            mv.addObject("list",amd);
+            mv.setViewName("medicalList");
+        }catch(Exception e){
+            e.printStackTrace();
+        }
 
-        return new ModelAndView("medicalList");
+        return mv;
     }
+
 
 }
