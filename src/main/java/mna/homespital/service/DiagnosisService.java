a11490882312f1effd8831beda7796ce5e9e75ad
Blation <kisn3089@naver.com>
2022-04-01 18:27:30 +0900

    create DiagnosisService and Impl

--- src/main/java/mna/homespital/service/DiagnosisService.java
+++ src/main/java/mna/homespital/service/DiagnosisService.java
@@ -1,2 +1,10 @@
-package mna.homespital.service;public interface diagnosisService {
+package mna.homespital.service;
+
+import mna.homespital.dto.Diagnosis;
+
+
+public interface DiagnosisService {
+
+//  진료차트폼 ( 인성 )
+    void insertDiagnosis(Diagnosis diagnosis) throws Exception;
 }
--- src/main/java/mna/homespital/service/DiagnosisServiceImpl.java
+++ src/main/java/mna/homespital/service/DiagnosisServiceImpl.java
@@ -1,2 +1,19 @@
-package mna.homespital.service;public class diagnosisServiceImpl {
+package mna.homespital.service;
+
+import mna.homespital.dao.DiagnosisDAO;
+import mna.homespital.dto.Diagnosis;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DiagnosisServiceImpl implements  DiagnosisService {
+
+    @Autowired
+    DiagnosisDAO diagnosisDAO;
+
+//    ( 인성 )
+    @Override
+    public void insertDiagnosis(Diagnosis diagnosis) throws Exception {
+        diagnosisDAO.insertDiagnosis(diagnosis);
+    }
 }
