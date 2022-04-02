a85dbe71ed1e659b7cb1c1822336cc7a09cbbba0
sintaeyoung <86948066+sintaeyoung@users.noreply.github.com>
2022-04-02 13:35:15 +0900

    taeyoung name modify

--- src/main/java/mna/homespital/service/AllMedicalListService.java
+++ src/main/java/mna/homespital/service/AllMedicalListService.java
@@ -1,11 +1,11 @@
 package mna.homespital.service;
 
 
-import mna.homespital.dto.allMedical;
+import mna.homespital.dto.AllMedical;
 
 import java.util.List;
 
-public interface allMedicalListService {
-
-    public List<allMedical> allMedList() throws Exception;
+public interface AllMedicalListService {
+    //모든진료항목 출력 태영
+    public List<AllMedical> allMedList() throws Exception;
 }
