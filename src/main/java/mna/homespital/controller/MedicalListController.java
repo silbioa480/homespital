9783fcb1d51b0d5259ec0de454ff4b670e672b20
sintaeyoung <86948066+sintaeyoung@users.noreply.github.com>
2022-04-02 13:31:22 +0900

    태영 진료리스트컨트롤러수정

--- src/main/java/mna/homespital/controller/MedicalListController.java
+++ src/main/java/mna/homespital/controller/MedicalListController.java
@@ -2,35 +2,41 @@ package mna.homespital.controller;
 
 
 
-import mna.homespital.dto.allMedical;
-import mna.homespital.service.allMedicalListService;
+import mna.homespital.dto.AllMedical;
+import mna.homespital.service.AllMedicalListService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 
 @RestController
-public class medicalListController {
+public class MedicalListController {
 
     @Autowired
-    allMedicalListService allmedListService;
+    AllMedicalListService allmedListService;
+
+    //모든 진료항목 출력 (태영)
 
     @GetMapping("/list")
-    public ModelAndView medicalList(){
-        ModelAndView mv=new ModelAndView();
+    public ModelAndView allmedicalList(){
+        ModelAndView mv = new ModelAndView();
         try{
-            List<allMedical> amd=allmedListService.allMedList();
+            List<AllMedical> amd= allmedListService.allMedList();
+
             mv.addObject("list",amd);
-            mv.setViewName("medicalList");
+            mv.setViewName("user/userside/medicalList");
         }catch(Exception e){
             e.printStackTrace();
         }
-
         return mv;
     }
+
+
 
 
 }
