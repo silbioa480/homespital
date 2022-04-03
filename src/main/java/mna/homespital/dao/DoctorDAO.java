dace9cfe93770aeb656122449a3c11da63ad7e9e
ekjkkk153 <ekjkkk153@daum.net>
2022-04-03 17:16:28 +0900

    Doctor DAO, 서비스 파일 생성 및 의료진 선택에서 의료진 리스트 가져올 수 있게 함

--- src/main/java/mna/homespital/dao/DoctorDAO.java
+++ src/main/java/mna/homespital/dao/DoctorDAO.java
@@ -1,5 +1,15 @@
 package mna.homespital.dao;
 
-public class DoctorDAO {
-    
+import mna.homespital.dto.Doctor;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Mapper
+@Repository
+public interface DoctorDAO {
+    List<Doctor> queryDoctor(int startrow) throws Exception;
+
+    Integer doctorAmount() throws Exception;
 }
