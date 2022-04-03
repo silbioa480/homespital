c33db2ebb518cfc8396b56dd1e41e4577acaad96
Gayoung Song <98731537+gayoung106@users.noreply.github.com>
2022-04-04 00:52:10 +0900

    회원탈퇴 추가

--- src/main/java/mna/homespital/dao/MemberDAO.java
+++ src/main/java/mna/homespital/dao/MemberDAO.java
@@ -6,6 +6,7 @@ import mna.homespital.dto.Pharmacy;
 import mna.homespital.dto.User;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 @Mapper
@@ -24,5 +25,14 @@ public interface MemberDAO {
     //소연 : 약사(Pharmacy)정보 가져오기
     Pharmacy selectPharmacyDetail(int pharmacy_number) throws Exception;
 
+//    //가영: 회원정보 수정
+//    void updateMember(User user) throws Exception;
+    
+    //가영: 회원탈퇴
+    public void deleteMember(String user_email)throws Exception;
 
+    //가영: 비밀번호수정
+    public String pwCheck(String user_email)throws Exception;
+
+    public void pwUpdate(String user_email, String user_password)throws Exception;
 }
