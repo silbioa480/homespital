e3fedd3dfa84f2b0955a0154389b004836d67984
ekjkkk153 <ekjkkk153@daum.net>
2022-04-05 15:30:13 +0900

    빌링키 발급 구현 완료 - DB에 넣는 과정은 아니고 결제 기능도 됨(/billingkeyForm 접속 후 /test

--- src/main/java/mna/homespital/controller/BillingController.java
+++ src/main/java/mna/homespital/controller/BillingController.java
@@ -1,7 +1,8 @@
 package mna.homespital.controller;
 
-import com.siot.IamportRestClient.IamportClient;
+import mna.homespital.service.IamportService;
 import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -10,25 +11,29 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.Map;
 
 @Controller
 public class BillingController {
+    @Autowired
+    IamportService iamportService;
+
     @GetMapping("/billingkeyForm")
     public String billkeyForm() {
         return "/user/main/test_billingkeyapi";
+    }
+
+    @GetMapping("/payForm")
+    public String payForm() {
+        return "/user/main/test_payapi";
     }
 
     @ResponseBody
     @PostMapping("/test-billingkey")
     public ResponseEntity<String> testbkey(@RequestParam Map<String, String> params) {
         JSONObject authTokenResponseRAW = null;
-        String authTokenResponse = "";
+        String authToken = "";
         JSONObject billingKeyResponseRAW = null;
         String billingKeyResponse = "";
         HttpURLConnection conn = null;
@@ -49,109 +54,50 @@ public class BillingController {
 //                customer_uid}
 
         try {
-            URL url = new URL("https://api.iamport.kr/users/getToken");
-            conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("POST");
-            conn.setRequestProperty("Content-Type", "application/json");
-            conn.setUseCaches(false);
-            conn.setDoInput(true);
-            conn.setDoOutput(true); //OutputStream을 사용해서 인증키 데이터 전송
-            JSONObject authInfo = new JSONObject();
-            authInfo.put("imp_key", "1205515243185179");
-            authInfo.put("imp_secret", "76b56c6a7987b195fd8d6ad27a7ca825a452753f94920947d4a9169106e698ff41561dfc05479bd7");
-            System.out.println(authInfo.toString());
-            try (OutputStream os = conn.getOutputStream()) {
-                byte request_data[] = authInfo.toString().getBytes();
-                os.write(request_data);
-                os.flush();
-                os.close();
-            } catch (Exception e) {
-                throw new Exception();
-            }
-            conn.connect();
-
-            int authTokenCode = conn.getResponseCode(); // 인증 토큰을 이렇게 요청함
-//            if(authTokenCode != 200) throw new Exception("RESPONSE NOT 200");
-            System.out.println(authTokenCode);
-            try { // 인증 토큰을 받아옴
-                StringBuilder sb = new StringBuilder();
-                BufferedReader br = new BufferedReader(
-                        new InputStreamReader(
-                                conn.getInputStream(), "utf-8"));
-                String s = null;
-                while ((s = br.readLine()) != null) {
-//                    authTokenResponse = authTokenResponse + s;
-                    sb.append(s);
-                }
-                br.close();
-                authTokenResponseRAW = new JSONObject(sb.toString());
-                JSONObject r = authTokenResponseRAW.getJSONObject("response");
-                authTokenResponse = r.getString("access_token");
-
-            } catch (Exception e) {
-                throw new Exception();
-            }
-            System.out.println("authToken : " + authTokenResponse);
-            conn.disconnect();
-            conn = null;
-
+            // 인증 토큰 요청
+            authTokenResponseRAW = iamportService.getAuthToken();
+            authToken = authTokenResponseRAW.getString("access_token");
 
             // 빌링키 발급 요청
-
-            URL billingkeyUrl = new URL("https://api.iamport.kr/subscribe/customers/" + cardData.getString("customer_uid"));
-            conn = (HttpURLConnection) billingkeyUrl.openConnection();
-            conn.setRequestMethod("POST");
-//            conn.setRequestProperty("Accept", "application/json");
-            conn.setRequestProperty("Content-Type", "application/json");
-            conn.setRequestProperty("Authorization", authTokenResponse);
-            conn.setDoInput(true);
-            conn.setDoOutput(true); //OutputStream을 사용해서 카드정보 데이터 전송
-            try (OutputStream os = conn.getOutputStream()) {
-                byte request_data[] = cardData.toString().getBytes("utf-8");
-                os.write(request_data);
-            } catch (Exception e) {
-                throw new Exception();
-            }
-
-            int billingkeyCode = conn.getResponseCode();
-            System.out.println(billingkeyCode);
-            if (billingkeyCode != 200) throw new Exception("BILLING KEY RESPONSE NOT 200");
-
-            try { // 인증 토큰을 받아옴
-                BufferedReader br = new BufferedReader(
-                        new InputStreamReader(
-                                conn.getInputStream(), "utf-8"));
-                String s = null;
-                while ((s = br.readLine()) != null) {
-                    billingKeyResponse = billingKeyResponse + s;
-                }
-                br.close();
-            } catch (Exception e) {
-                throw new Exception();
-            }
-
-//        const { code, message } = issueBilling.data;
-//            if (code === 0) { // 빌링키 발급 성공
-//                res.send({ status: "success", message: "Billing has successfully issued" });
-//            } else { // 빌링키 발급 실패
-//                res.send({ status: "failed", message });
-//            }
-            return new ResponseEntity<String>(billingKeyResponse, HttpStatus.OK);
+            JSONObject billingKey = iamportService.getBillingKey(authToken, cardData);
+            return new ResponseEntity<String>(billingKey.toString(), HttpStatus.OK);
         } catch (Exception e) {
             e.printStackTrace();
             return new ResponseEntity<String>("ERROR", HttpStatus.BAD_REQUEST);
         }
     }
 
-    @ResponseBody
-    @PostMapping("/test-billingkey2")
-    public ResponseEntity<String> testbkey2() {
-        try {
-            IamportClient client = new IamportClient("1205515243185179", "76b56c6a7987b195fd8d6ad27a7ca825a452753f94920947d4a9169106e698ff41561dfc05479bd7");
+//    @ResponseBody
+//    @PostMapping("/test-billingkey2")
+//    public ResponseEntity<String> testbkey2() {
+//        try {
+//            IamportClient client = new IamportClient("1205515243185179", "76b56c6a7987b195fd8d6ad27a7ca825a452753f94920947d4a9169106e698ff41561dfc05479bd7");
+//
+//            return new ResponseEntity<String>("OK", HttpStatus.OK);
+//        } catch (Exception e) {
+//            return new ResponseEntity<String>("ERROR", HttpStatus.BAD_REQUEST);
+//        }
+//    }
 
-            return new ResponseEntity<String>("OK", HttpStatus.OK);
+    @ResponseBody
+    @PostMapping("/test-pay")
+    public ResponseEntity<String> testpay1(@RequestParam Map<String, String> params) {
+        JSONObject r = null;
+        try {
+//            String authToken = params.get("authToken");
+            String customer_uid = params.get("customer_uid");
+            String merchant_uid = params.get("merchant_uid");
+            int amount = Integer.parseInt(params.get("amount"));
+            String name = params.get("name");
+            System.out.println(1);
+            JSONObject at = iamportService.getAuthToken();
+            System.out.println(at.getString("access_token"));
+            r = iamportService.pay(at.getString("access_token"), customer_uid, merchant_uid, amount, name);
+            System.out.println(3);
+            return new ResponseEntity<String>(r.toString(), HttpStatus.OK);
         } catch (Exception e) {
-            return new ResponseEntity<String>("ERROR", HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<String>("[ERROR]" + r.toString(), HttpStatus.BAD_REQUEST);
         }
     }
 }
+
