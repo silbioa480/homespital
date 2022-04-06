0ec8897c55843aa701f678050cf70329c6959d73
sintaeyoung <86948066+sintaeyoung@users.noreply.github.com>
2022-04-06 09:46:10 +0900

    taeyoung service modify

--- src/main/java/mna/homespital/service/AllMedicalListService.java
+++ src/main/java/mna/homespital/service/AllMedicalListService.java
@@ -8,4 +8,7 @@ import java.util.List;
 public interface AllMedicalListService {
     //모든진료항목 출력 태영
     public List<AllMedical> allMedList() throws Exception;
+    //진료과 검색 태영
+    public List<AllMedical> searchMed(String keyward) throws Exception;
+
 }
