b9faf6d5d90e8285438035b9489270b7b2393cc1
silbioa480 <silbioa480@gmail.com>
2022-04-06 10:07:20 +0900

    fix conflict

--- src/main/java/mna/homespital/service/AllMedicalListService.java
+++ src/main/java/mna/homespital/service/AllMedicalListService.java
@@ -6,14 +6,11 @@ import mna.homespital.dto.AllMedical;
 import java.util.List;
 
 public interface AllMedicalListService {
-<<<<<<< HEAD
-    //모든진료항목 출력 태영
-    public List<AllMedical> allMedList() throws Exception;
-    //진료과 검색 태영
-    public List<AllMedical> searchMed(String keyward) throws Exception;
 
-=======
   //모든진료항목 출력 태영
   public List<AllMedical> allMedList() throws Exception;
->>>>>>> 2dfa99d49553e26f7ca30b4268d8839824508159
+
+  //진료과 검색 태영
+  public List<AllMedical> searchMed(String keyward) throws Exception;
+
 }
--- src/main/java/mna/homespital/service/AllMedicalListServiceimpl.java
+++ src/main/java/mna/homespital/service/AllMedicalListServiceimpl.java
@@ -16,16 +16,13 @@ public class AllMedicalListServiceimpl implements AllMedicalListService {
 
   //모든진료항목 출력 태영
 
-    @Override
-    public List<AllMedical> allMedList() throws Exception {
-        return allmdDAO.allMedicalList();
-    }
+  @Override
+  public List<AllMedical> allMedList() throws Exception {
+    return allmdDAO.allMedicalList();
+  }
 
-<<<<<<< HEAD
-    @Override
-    public List<AllMedical> searchMed(String keyward) throws Exception {
-        return allmdDAO.searchMedical(keyward);
-    }
-=======
->>>>>>> 2dfa99d49553e26f7ca30b4268d8839824508159
+  @Override
+  public List<AllMedical> searchMed(String keyward) throws Exception {
+    return allmdDAO.searchMedical(keyward);
+  }
 }
