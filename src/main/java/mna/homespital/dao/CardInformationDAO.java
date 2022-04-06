bb40505b587a27e5fb7494c1967ee5ac54279cc0
ekjkkk153 <ekjkkk153@daum.net>
2022-04-06 11:24:10 +0900

    카드정보 DAO - 카드 정보 하나만 가져오기, 카드 삭제하기

--- src/main/java/mna/homespital/dao/CardInformationDAO.java
+++ src/main/java/mna/homespital/dao/CardInformationDAO.java
@@ -1,9 +1,20 @@
 package mna.homespital.dao;
 
+import mna.homespital.dto.Card_Information;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Map;
 
 @Mapper
 @Repository
 public interface CardInformationDAO {
+    List<Card_Information> queryMyCards(int card_owner_number) throws Exception;
+
+    Card_Information queryMyCard(Map<String, String> params) throws Exception;
+
+    void insertMyCard(Card_Information param) throws Exception;
+
+    void deleteMyCard(String customer_uid) throws Exception;
 }
