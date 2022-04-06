15bd7cd5a411c480d49b6f461ab2fd7f11f2a9e1
ekjkkk153 <ekjkkk153@daum.net>
2022-04-06 10:42:07 +0900

    카드정보 DAO 껍데기 생성

---
+++ src/main/java/mna/homespital/dao/CardInformationDAO.java
@@ -0,0 +1,9 @@
+package mna.homespital.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+@Mapper
+@Repository
+public interface CardInformationDAO {
+}
