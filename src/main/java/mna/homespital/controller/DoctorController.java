518f63c7ba5cea622f3f847e72dbbb2393ddf34c
Blation <kisn3089@naver.com>
2022-04-08 03:10:24 +0900

    임시로 닥터 컨트롤러에 약사 진료 리스트 작업

--- src/main/java/mna/homespital/controller/DoctorController.java
+++ src/main/java/mna/homespital/controller/DoctorController.java
@@ -1,15 +1,57 @@
 package mna.homespital.controller;
 
+import mna.homespital.dto.Diagnosis;
+import mna.homespital.service.MedicalListService;
+import mna.homespital.service.PharService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 @Controller
-@RequestMapping("/admin")
+@RequestMapping("/")
 public class DoctorController {
 
-    @GetMapping("/")
-    public String DoctorMain() {
-        return "admin/main/index";
+    @Autowired
+    HttpSession session;
+
+    @Autowired
+    PharService pharService;
+
+    //나의진료내역 (인성)
+    @GetMapping("/customerList")
+    public String customerList(HttpSession session, Model m) throws Exception {
+        try {
+//            String email = (String) session.getAttribute("email");
+//            String name = (String) session.getAttribute("name");
+//            int searchNumber = pharService.getNumberByEmail(email);
+            Diagnosis diagnosis = new Diagnosis();
+//            diagnosis.setUser_number(searchNumber);
+            m.addAttribute("diagnosis", diagnosis);
+        } catch (Exception e) {
+            System.out.println("Catch() join");
+            e.printStackTrace();
+            return "common/err";
+        }
+        return "admin/phar/customerList";
     }
+
+    //진료내역 리스트 출력 (준근)
+    @ResponseBody
+    @GetMapping("/pharCustomerRecordsList")
+    public ArrayList<HashMap<String, Object>> pharCustomerRecordsList(@RequestParam int phar_number) {
+        ArrayList<HashMap<String, Object>> customerList = new ArrayList<>();
+        try {
+            customerList = pharService.pharCustomerRecordsList(phar_number);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return customerList;
+    }
+
 }
--- src/main/java/mna/homespital/dao/PharmacyDAO.java
+++ src/main/java/mna/homespital/dao/PharmacyDAO.java
@@ -5,11 +5,21 @@ import mna.homespital.dto.Pharmacy;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 @Repository
 @Mapper
 public interface PharmacyDAO {
 
     //소연 : 약사(Pharmacy)정보 가져오기
     Pharmacy PharInfo(int pharmacy_number) throws Exception;
+
+//  인성: 약사 세션 email로 number 가져오기
+    int getNumberByEmail(String user_email) throws Exception;
+
+    // 인성: customerList 출력
+    ArrayList<HashMap<String, Object>> pharCustomerRecordsList(int phar_number) throws Exception;
+
 
 }
