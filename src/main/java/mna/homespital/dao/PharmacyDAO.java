69103a07bcead94d7831ba091b6b25ae08141e96
INYONGSIK <97017840+INYONGSIK@users.noreply.github.com>
2022-04-07 22:01:22 +0900

    용식: 약국dao  이메일로 정보가져오기 정보넣기용 추가

--- src/main/java/mna/homespital/dao/PharmacyDAO.java
+++ src/main/java/mna/homespital/dao/PharmacyDAO.java
@@ -12,4 +12,11 @@ public interface PharmacyDAO {
     //소연 : 약사(Pharmacy)정보 가져오기
     Pharmacy PharInfo(int pharmacy_number) throws Exception;
 
+    //용식: 이메일로 약사 정보 가져오기
+    Pharmacy PharmacyQueryMember(String pharmacy_email) throws Exception;
+
+    //용식: 약사 정보 넣기
+    void insertPharmacyMember(Pharmacy pharmacy) throws Exception;
+
+
 }
