aabf5f3a93e320499be0d888fef9fe1907a2806f
silbioa480 <silbioa480@gmail.com>
2022-04-07 18:17:33 +0900

    add /doctorList/distance controller

--- src/main/java/mna/homespital/controller/RootController.java
+++ src/main/java/mna/homespital/controller/RootController.java
@@ -25,198 +25,208 @@ import java.util.UUID;
 @RequestMapping("/")
 public class RootController {
 
-    @Autowired
-    private ServletContext servletContext;
+  @Autowired
+  private ServletContext servletContext;
 
-    @Autowired
-    HttpSession session;
+  @Autowired
+  HttpSession session;
 
-    @Autowired
-    MedicalListService medicalListService;
+  @Autowired
+  MedicalListService medicalListService;
 
-    @Autowired
-    DiagnosisService diagnosisService;
+  @Autowired
+  DiagnosisService diagnosisService;
 
-    @Autowired
-    DoctorService doctorService;
+  @Autowired
+  DoctorService doctorService;
 
-    @Autowired
-    MemberService memberService;
+  @Autowired
+  MemberService memberService;
 
-    @Autowired
-    UserService userService;
+  @Autowired
+  UserService userService;
 
-    public RootController() {
+  public RootController() {
+  }
+
+  @GetMapping("/")
+  public ModelAndView index() {
+    return new ModelAndView("user/main/index");
+  }
+
+  //환자로그인
+  @GetMapping("/loginForm")
+  public ModelAndView loginForm() {
+    return new ModelAndView("user/main/loginForm");
+  }
+
+
+  //환자회원가입
+  @GetMapping("/joinForm")
+  public ModelAndView joinForm() {
+    return new ModelAndView("user/userside/joinForm");
+  }
+
+  //약국회원가입
+  @GetMapping("/pharmacyJoinForm")
+  public ModelAndView phamacyJoinForm() {
+    return new ModelAndView("admin/pharside/joinForm");
+  }
+
+  //환자회원정보수정
+  @GetMapping("/modifyForm")
+  public ModelAndView modifyForm() {
+    ModelAndView mav = new ModelAndView("user/userside/modifyForm");
+    String email = (String) session.getAttribute("email");
+    try {
+      User user = memberService.queryMember(email);
+      String juminNum = user.getUser_registration_number();
+      user.setUser_registration_number(juminNum.replaceAll(".{6}$", "******"));
+
+      if (user == null) {
+        mav.setViewName("user/main/loginForm");
+      } else {
+        mav.addObject("user", user);
+        System.out.println(user.toString());
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
     }
+    return mav;
+  }
 
-    @GetMapping("/")
-    public ModelAndView index() {
-        return new ModelAndView("user/main/index");
+  //환자비밀번호확인(정보수정 전)
+  @GetMapping("/pwCheck")
+  public ModelAndView pwCheck() {
+    String email = (String) session.getAttribute("email");
+
+    if (email == null) {
+      return new ModelAndView("user/main/index");
     }
+    return new ModelAndView("user/userside/pwCheck");
+  }
 
-    //환자로그인
-    @GetMapping("/loginForm")
-    public ModelAndView loginForm() {
-        return new ModelAndView("user/main/loginForm");
+  //환자회원탈퇴
+  @GetMapping("/delete")
+  public ModelAndView deleteForm() {
+    String email = (String) session.getAttribute("email");
+
+    if (email == null) {
+      return new ModelAndView("user/main/index");
     }
+    return new ModelAndView("user/userside/deleteForm");
+  }
 
+  //환자비밀번호 찾기
+  @GetMapping("/findpwForm")
+  public ModelAndView findpwForm() {
+    return new ModelAndView("user/main/findpwForm");
+  }
 
-    //환자회원가입
-    @GetMapping("/joinForm")
-    public ModelAndView joinForm() {
-        return new ModelAndView("user/userside/joinForm");
-    }
+  //의료진 찾기
+  //    @SuppressWarnings("deprecation") // 의사 목업코드를 넣을때 쓴 코드. DAO로 실제 DB를 받아올 수 있다면 떼도 됨
+  @GetMapping("/doctorList")
+  public ModelAndView doctorList(@RequestParam(required = false, defaultValue = "1") int page) throws Exception {
+    ModelAndView mv = new ModelAndView("user/userside/doctorList");
+    PageInfo pageInfo = new PageInfo();
+    List<Doctor> doctorList = doctorService.getDocList(page, pageInfo);
 
-    //약국회원가입
-    @GetMapping("/pharmacyJoinForm")
-    public ModelAndView phamacyJoinForm() {
-        return new ModelAndView("admin/pharside/joinForm");
-    }
+    mv.addObject("doctorList", doctorList);
+    mv.addObject("pageInfo", pageInfo);
+    return mv;
+  }
 
-    //환자회원정보수정
-    @GetMapping("/modifyForm")
-    public ModelAndView modifyForm() {
-        ModelAndView mav = new ModelAndView("user/userside/modifyForm");
-        String email = (String) session.getAttribute("email");
-        try {
-            User user = memberService.queryMember(email);
-            String juminNum = user.getUser_registration_number();
-            user.setUser_registration_number(juminNum.replaceAll(".{6}$", "******"));
+  @GetMapping("/doctorList/distance")
+  public ModelAndView doctorListByDistance(@RequestBody List<Doctor> doctorList) {
+    ModelAndView mv = new ModelAndView("user/userside/doctorList");
+    PageInfo pageInfo = new PageInfo();
 
-            if (user == null) {
-                mav.setViewName("user/main/loginForm");
-            } else {
-                mav.addObject("user", user);
-                System.out.println(user.toString());
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return mav;
-    }
+    mv.addObject("doctorList", doctorList);
+    mv.addObject("pageInfo", pageInfo);
+    return mv;
+  }
 
-    //환자비밀번호확인(정보수정 전)
-    @GetMapping("/pwCheck")
-    public ModelAndView pwCheck() {
-        String email = (String) session.getAttribute("email");
-
-        if (email == null) {
-            return new ModelAndView("user/main/index");
-        }
-        return new ModelAndView("user/userside/pwCheck");
-    }
-
-    //환자회원탈퇴
-    @GetMapping("/delete")
-    public ModelAndView deleteForm() {
-        String email = (String) session.getAttribute("email");
-
-        if (email == null) {
-            return new ModelAndView("user/main/index");
-        }
-        return new ModelAndView("user/userside/deleteForm");
-    }
-
-    //환자비밀번호 찾기
-    @GetMapping("/findpwForm")
-    public ModelAndView findpwForm() {
-        return new ModelAndView("user/main/findpwForm");
-    }
-
-    //의료진 찾기
-    //    @SuppressWarnings("deprecation") // 의사 목업코드를 넣을때 쓴 코드. DAO로 실제 DB를 받아올 수 있다면 떼도 됨
-    @GetMapping("/doctorList")
-    public ModelAndView doctorList(@RequestParam(required = false, defaultValue = "1") int page) throws Exception {
-        ModelAndView mv = new ModelAndView("user/userside/doctorList");
-        PageInfo pageInfo = new PageInfo();
-        List<Doctor> doctorList = doctorService.getDocList(page, pageInfo);
-
-        mv.addObject("doctorList", doctorList);
-        mv.addObject("pageInfo", pageInfo);
-        return mv;
-    }
-
-    //원하는 의사명 및 병원명 찾기 태영
-    @PostMapping("/dohSearch")
-    public ModelAndView dohSearch(@RequestParam(value="dhSearch") String dhSearch){
-        ModelAndView mv=new ModelAndView();
-        try{
-            List<Doctor> doc=doctorService.getSearchDoh(dhSearch);
+  //원하는 의사명 및 병원명 찾기 태영
+  @PostMapping("/dohSearch")
+  public ModelAndView dohSearch(@RequestParam(value = "dhSearch") String dhSearch) {
+    ModelAndView mv = new ModelAndView();
+    try {
+      List<Doctor> doc = doctorService.getSearchDoh(dhSearch);
 //            List<Doctor> docList = doctorService.getDocList(page, pageInfo);
-            mv.addObject("doctorList",doc);
-            mv.setViewName("user/userside/doctorList");
-        }catch(Exception e){
-            e.printStackTrace();
-        }
-        return mv;
+      mv.addObject("doctorList", doc);
+      mv.setViewName("user/userside/doctorList");
+    } catch (Exception e) {
+      e.printStackTrace();
     }
-    //진료차트 쓰기
+    return mv;
+  }
+  //진료차트 쓰기
 //  @GetMapping("/appointmentForm/{doc}")
 //  public ModelAndView appointmentForm(@PathVariable int doc) {
 
-    @GetMapping("/appointmentForm/{doctor_number}")
-    public ModelAndView appointmentForm(@PathVariable int doctor_number) throws Exception {
-        ModelAndView mv = new ModelAndView("user/userside/appointmentForm");
+  @GetMapping("/appointmentForm/{doctor_number}")
+  public ModelAndView appointmentForm(@PathVariable int doctor_number) throws Exception {
+    ModelAndView mv = new ModelAndView("user/userside/appointmentForm");
 //        int doctor_number = 1;
-        // page
-        try {
-            Doctor doctor = doctorService.getDocInfo(doctor_number);
-            doctor.setDoctor_password("");
+    // page
+    try {
+      Doctor doctor = doctorService.getDocInfo(doctor_number);
+      doctor.setDoctor_password("");
 
-            System.out.println(doctor.getDoctor_name());
-            mv.addObject("doctor", doctor);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        String email = (String) session.getAttribute("email");
-
-        try {
-            User user = memberService.findByEmail(email);
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return mv;
+      System.out.println(doctor.getDoctor_name());
+      mv.addObject("doctor", doctor);
+    } catch (Exception e) {
+      e.printStackTrace();
     }
+    String email = (String) session.getAttribute("email");
 
-    //진료예약   ( 인성 )
-    @PostMapping("/appointmentForm")
-    public String appointment(Diagnosis diagnosis, MultipartFile[] diagnosisImgNames,
-                              Model model, HttpServletRequest request, HttpServletResponse response) {
-        try {
-            // 사진 업로드
-            String fileNameArr = "";
+    try {
+      User user = memberService.findByEmail(email);
 
-            for (int i = 0; i < diagnosisImgNames.length; i++) {
-                String diagnosisImg = diagnosisImgNames[i].getOriginalFilename();
-                String path = servletContext.getRealPath("/resources/img/uploadImg/");
-                String filename = UUID.randomUUID().toString() + "." + diagnosisImg.substring(diagnosisImg.lastIndexOf('.') + 1);
-                File destFile = new File(path + filename);
-                diagnosisImgNames[i].transferTo(destFile);
-                diagnosisImg = filename;
-                fileNameArr += (diagnosisImg + ", ");
-            }
-
-            // DB insert
-            diagnosis.setDiagnosis_image_name(fileNameArr.toString());
-            diagnosisService.insertDiagnosis(diagnosis);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return "/appointmentSuccess";
+    } catch (Exception e) {
+      e.printStackTrace();
     }
+    return mv;
+  }
 
-    // 관리자 메인 페이지 임시로 만들어놈 ( 인성 )
-    @GetMapping("/adminIndex")
-    public ModelAndView adminIndex() {
-        return new ModelAndView("admin/main/adminIndex");
-    }
+  //진료예약   ( 인성 )
+  @PostMapping("/appointmentForm")
+  public String appointment(Diagnosis diagnosis, MultipartFile[] diagnosisImgNames,
+                            Model model, HttpServletRequest request, HttpServletResponse response) {
+    try {
+      // 사진 업로드
+      String fileNameArr = "";
 
-    //약국 메인페이지 태영
-    @GetMapping("/pharmacyIndex")
-    public ModelAndView pharmacyIndex(){
-        ModelAndView mv =new ModelAndView();
-        mv.setViewName("admin/pharside/pharmacyIndex");
-        return mv;
+      for (int i = 0; i < diagnosisImgNames.length; i++) {
+        String diagnosisImg = diagnosisImgNames[i].getOriginalFilename();
+        String path = servletContext.getRealPath("/resources/img/uploadImg/");
+        String filename = UUID.randomUUID().toString() + "." + diagnosisImg.substring(diagnosisImg.lastIndexOf('.') + 1);
+        File destFile = new File(path + filename);
+        diagnosisImgNames[i].transferTo(destFile);
+        diagnosisImg = filename;
+        fileNameArr += (diagnosisImg + ", ");
+      }
+
+      // DB insert
+      diagnosis.setDiagnosis_image_name(fileNameArr.toString());
+      diagnosisService.insertDiagnosis(diagnosis);
+    } catch (Exception e) {
+      e.printStackTrace();
     }
+    return "/appointmentSuccess";
+  }
+
+  // 관리자 메인 페이지 임시로 만들어놈 ( 인성 )
+  @GetMapping("/adminIndex")
+  public ModelAndView adminIndex() {
+    return new ModelAndView("admin/main/adminIndex");
+  }
+
+  //약국 메인페이지 태영
+  @GetMapping("/pharmacyIndex")
+  public ModelAndView pharmacyIndex() {
+    ModelAndView mv = new ModelAndView();
+    mv.setViewName("admin/pharside/pharmacyIndex");
+    return mv;
+  }
 }
