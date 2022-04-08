9cf0e3fe6947435ce1b43fada8dbc59e0bf6d0e0
Gayoung Song <98731537+gayoung106@users.noreply.github.com>
2022-04-08 16:01:12 +0900

    의사 비밀번호 확인창

--- src/main/java/mna/homespital/service/DoctorService.java
+++ src/main/java/mna/homespital/service/DoctorService.java
@@ -2,6 +2,8 @@ package mna.homespital.service;
 
 import mna.homespital.dto.Doctor;
 import mna.homespital.dto.PageInfo;
+import mna.homespital.dto.User;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -14,6 +16,12 @@ public interface DoctorService {
     
 //가영: 의사 아이디중복확인
     boolean emailCheck(String email) throws Exception;
+
+    //가영: 비밀번호 확인(회원정보변경 전 페이지)
+    Doctor findByEmail(String email) throws Exception;
+
+    //가영: 유저정보 가져오기
+    Doctor doctorQueryMember(String email) throws Exception;
 
     // 의료진 찾기 - 의료진 목록 보기 (훈)
     public List<Doctor> getDocList(int page, PageInfo pageInfo) throws Exception;
