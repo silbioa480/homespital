eca8b7de43a72536884f8b5c43dacd3aaf0dbce4
THLAND <upperrr@gmail.com>
2022-04-08 17:05:00 +0900

    diagnosis dto수정

--- src/main/java/mna/homespital/dto/Diagnosis.java
+++ src/main/java/mna/homespital/dto/Diagnosis.java
@@ -22,14 +22,19 @@ public class Diagnosis {
     private int is_delivery;
     private String billing_key;
     private String doctor_opinion;
-    private String diagnosis_address; //4/7추가. appointmentForm에 is delievry==1 (배송)의 경우 주소 API이용하여 받는 사람 주소를 넣는데, 그때 DB에 저장하는 주소
+
+    private String pharmacy_name; //4/8추가 by 소연
+    private String pharmacy_address; //4/8추가 by 소연
+    private String pharmacy_phone; //4/8추가 by 소연
+    private String user_address; //4/7추가. appointmentForm에 is delievry==1 (배송)의 경우 주소 API이용하여 받는 사람 주소를 넣는데, 그때 DB에 저장하는 주소
+
     private Date create_date;
     private Date update_date;
 
     public Diagnosis() {
     }
 
-    public Diagnosis(int diagnosis_number, int user_number, int doctor_number, int pharmacy_number, String diagnosis_type, int diagnosis_time, int diagnosis_wait_number, String diagnosis_content, String diagnosis_image_name, int diagnosis_status, int is_diagnosis_upload, int is_prescription_upload, String diagnosis_file_name, String prescription_file_name, int diagnosis_money, int prescription_money, int is_delivery, String billing_key, String doctor_opinion, String diagnosis_address, Date create_date, Date update_date) {
+    public Diagnosis(int diagnosis_number, int user_number, int doctor_number, int pharmacy_number, String diagnosis_type, int diagnosis_time, int diagnosis_wait_number, String diagnosis_content, String diagnosis_image_name, int diagnosis_status, int is_diagnosis_upload, int is_prescription_upload, String diagnosis_file_name, String prescription_file_name, int diagnosis_money, int prescription_money, int is_delivery, String billing_key, String doctor_opinion, String pharmacy_name, String pharmacy_address, String pharmacy_phone, String user_address, Date create_date, Date update_date) {
         this.diagnosis_number = diagnosis_number;
         this.user_number = user_number;
         this.doctor_number = doctor_number;
@@ -48,8 +53,11 @@ public class Diagnosis {
         this.prescription_money = prescription_money;
         this.is_delivery = is_delivery;
         this.billing_key = billing_key;
-        this.diagnosis_address = diagnosis_address;
         this.doctor_opinion = doctor_opinion;
+        this.pharmacy_name = pharmacy_name;
+        this.pharmacy_address = pharmacy_address;
+        this.pharmacy_phone = pharmacy_phone;
+        this.user_address = user_address;
         this.create_date = create_date;
         this.update_date = update_date;
     }
@@ -206,12 +214,36 @@ public class Diagnosis {
         this.doctor_opinion = doctor_opinion;
     }
 
-    public String getDiagnosis_address() {
-        return diagnosis_address;
+    public String getPharmacy_name() {
+        return pharmacy_name;
     }
 
-    public void setDiagnosis_address(String diagnosis_address) {
-        this.diagnosis_address = diagnosis_address;
+    public void setPharmacy_name(String pharmacy_name) {
+        this.pharmacy_name = pharmacy_name;
+    }
+
+    public String getPharmacy_address() {
+        return pharmacy_address;
+    }
+
+    public void setPharmacy_address(String pharmacy_address) {
+        this.pharmacy_address = pharmacy_address;
+    }
+
+    public String getPharmacy_phone() {
+        return pharmacy_phone;
+    }
+
+    public void setPharmacy_phone(String pharmacy_phone) {
+        this.pharmacy_phone = pharmacy_phone;
+    }
+
+    public String getUser_address() {
+        return user_address;
+    }
+
+    public void setUser_address(String user_address) {
+        this.user_address = user_address;
     }
 
     public Date getCreate_date() {
