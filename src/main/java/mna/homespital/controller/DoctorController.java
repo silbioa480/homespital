87b3d71e0bb6c77f08f42fc54b21b6b7001749aa
sintaeyoung <86948066+sintaeyoung@users.noreply.github.com>
2022-04-08 17:06:00 +0900

    taeyoung doctorcontroller modify

--- src/main/java/mna/homespital/controller/DoctorController.java
+++ src/main/java/mna/homespital/controller/DoctorController.java
@@ -2,7 +2,9 @@ package mna.homespital.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -14,6 +16,19 @@ public class DoctorController {
         return "admin/main/index";
     }
 
+//    @ResponseBody
+//    @PostMapping("/numberCheck")
+//    public String  numberCheck(){
+//
+//    }
 
+
+    //의사측 진료내역리스트 태영
+    @GetMapping("/doctor/customerList")
+    public ModelAndView doccustomerList(){
+        ModelAndView mv=new ModelAndView();
+        mv.setViewName("admin/doctor/customerList");
+        return mv;
+    }
 
 }
