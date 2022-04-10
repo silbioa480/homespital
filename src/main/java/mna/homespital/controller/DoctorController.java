04f24c0ffe1fe33c45c50c26dbb13bfc31fba29d
Blation <kisn3089@naver.com>
2022-04-11 00:35:34 +0900

    DoctorController session으로 이메일 말고 닥터 객체를 바로 가져옴

--- src/main/java/mna/homespital/controller/DoctorController.java
+++ src/main/java/mna/homespital/controller/DoctorController.java
@@ -55,7 +55,9 @@ public class DoctorController {
 
     try {
       doctorService.docLogin(doctor_email, doctor_password);
-      session.setAttribute("email", doctor_email);
+      Doctor doctor = doctorService.searchDocId(doctor_email);
+      doctor.setDoctor_password("");
+      session.setAttribute("doctor", doctor);
       return "redirect:/doctor/";
     } catch (Exception e) {
       model.addAttribute("err", e.getMessage());
@@ -71,23 +73,6 @@ public class DoctorController {
     }
   }
 
-  //나의진료내역 (인성)
-  @GetMapping("/customerList")
-  public String customerList(HttpSession session, Model m) throws Exception {
-    try {
-//            String email = (String) session.getAttribute("email");
-//            String name = (String) session.getAttribute("name");
-//            int searchNumber = pharService.getNumberByEmail(email);
-      Diagnosis diagnosis = new Diagnosis();
-//            diagnosis.setUser_number(searchNumber);
-      m.addAttribute("diagnosis", diagnosis);
-    } catch (Exception e) {
-      System.out.println("Catch() join");
-      e.printStackTrace();
-      return "common/err";
-    }
-    return "admin/phar/customerList";
-  }
 
   //가영:의사 회원가입
   @PostMapping("/doctorJoin.do")
@@ -144,7 +129,7 @@ public class DoctorController {
     try {
       System.out.println("여기들어오라ㅏ ㅇㅇㅇㅇㅇ");
       doctorService.join(doctor);
-      mv.setViewName("redirect:/loginForm");
+      mv.setViewName("redirect:/doctor/docLogin");
     } catch (Exception e) {
       e.printStackTrace();
       mv.setViewName("redirect:/doctorJoin");
@@ -155,7 +140,7 @@ public class DoctorController {
 
   //가영: 의사 이메일 중복확인
 
-  @PostMapping("/DoctorEmailoverlap")
+  @PostMapping("/Emailoverlap")
   @ResponseBody
   public boolean emailOverLap(@RequestParam String email) {
     boolean result = false;
@@ -200,10 +185,11 @@ public class DoctorController {
   public String docMedicalList(HttpSession session, Model m) throws Exception {
     System.out.println("docMedicalList() Join");
     try {
-      String email = (String) session.getAttribute("email");
-      int serarchDocNum = doctorService.searchDocId(email);
+      Doctor doctor = (Doctor) session.getAttribute("doctor");
+//      int serarchDocNum = doctorService.searchDocId(email);
       Diagnosis diagnosis = new Diagnosis();
-      diagnosis.setDoctor_number(serarchDocNum);
+      diagnosis.setDoctor_number(doctor.getDoctor_number());
+      //      이부분 준근님에게 물어보기
       m.addAttribute("diagnosis", diagnosis);
     } catch (Exception e) {
       e.printStackTrace();
