c1b35ca06ecdc184d3053240d043c0d0ec5778e4
INYONGSIK <97017840+INYONGSIK@users.noreply.github.com>
2022-04-10 21:55:48 +0900

    약사 dao 추가

--- src/main/java/mna/homespital/dao/PharmacyDAO.java
+++ src/main/java/mna/homespital/dao/PharmacyDAO.java
@@ -15,7 +15,7 @@ public interface PharmacyDAO {
     //소연 : 약사(Pharmacy)정보 가져오기
     Pharmacy PharInfo(int pharmacy_number) throws Exception;
 
-//  인성: 약사 세션 email로 number 가져오기
+    //  인성: 약사 세션 email로 number 가져오기
     int getNumberByEmail(String user_email) throws Exception;
 
     // 인성: customerList 출력
@@ -26,5 +26,6 @@ public interface PharmacyDAO {
 
     //용식: 약사 정보 넣기
     void insertPharmacyMember(Pharmacy pharmacy) throws Exception;
+
 
 }
