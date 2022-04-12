b2a92683e2af09d0bef71971b3a736bffa165d44
sintaeyoung <86948066+sintaeyoung@users.noreply.github.com>
2022-04-12 21:49:33 +0900

    taeyoung doctorcontroller modify

--- src/main/java/mna/homespital/controller/DoctorController.java
+++ src/main/java/mna/homespital/controller/DoctorController.java
@@ -96,7 +96,7 @@ public class DoctorController {
 
     //가영:의사 회원가입
     @PostMapping("/doctorJoin.do")
-    public ModelAndView doctorJoin(HttpServletRequest request) {
+    public ModelAndView doctorJoin(HttpServletRequest request, MultipartFile doctorImgNames, MultipartFile hospitalImgNames, HttpServletResponse response) {
         Doctor doctor = new Doctor();
         doctor.setDoctor_email(request.getParameter("doctor_email"));
         doctor.setDoctor_password(request.getParameter("doctor_password"));
@@ -180,7 +180,7 @@ public class DoctorController {
         return mv;
     }
 
-    //가영: 의사 이메일 중복확인
+//    가영: 의사 이메일 중복확인
 
     @PostMapping("/Emailoverlap")
     @ResponseBody
