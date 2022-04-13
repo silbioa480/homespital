14708cc4467fe6e1a4e20c32a3229d935a3c11e2
THLAND <upperrr@gmail.com>
2022-04-13 16:48:00 +0900

    test6

--- src/main/java/mna/homespital/component/CamComponent.java
+++ src/main/java/mna/homespital/component/CamComponent.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
 
-import javax.websocket.OnClose;
 import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
@@ -12,48 +11,33 @@ import javax.websocket.server.ServerEndpoint;
 
 import org.springframework.stereotype.Component;
 
-
 @Component
-@ServerEndpoint("/WebSocketCam/{roomNum}")
+@ServerEndpoint("/WebSocketCam/{id}")
 public class CamComponent {
-
+	
 	ArrayList<Session> list;
-
+	
 	@OnOpen
 	public void open(Session session) {
 		list=SessionList.getInstance().list;
-		if (list.size()>=2) {
-			close(session);
-		} else {
-			synchronized (list) {
-				System.out.println(list.size());
-				list.add(session);
-			}
-			System.out.println(session.getId()+"화상 접속 ok :"+list.size());
-		}
+		System.out.println(list.size());
+		System.out.println("화상 접속 ok");
 	}
-
+	
 	@OnMessage
 	public void receiveBinary(ByteBuffer bb, boolean last,Session session) {
-		try {			
-			session.getBasicRemote().sendBinary(bb,last);
+		try {
+			for (Session peer:list) {
+				if(!session.getId().equals(peer.getId())) {
+					session.getBasicRemote().sendBinary(bb,last);
+				}
+			}
 		} catch (IOException e) {
-			e.printStackTrace();
 			try {
 				session.close();
 			} catch (IOException e1) {
-				e1.printStackTrace();
+				
 			}
 		}
 	}
-
-	@OnClose
-	public void close(Session session) {
-		System.out.println(session.getId()+" 접속 end");
-		synchronized (list) {
-			list.remove(session);
-		}
-
-	}
-
 }
