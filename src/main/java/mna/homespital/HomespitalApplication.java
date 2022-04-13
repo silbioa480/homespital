bddcbbcd1c846ccba574dd8c197d5fb9672becd6
THLAND <upperrr@gmail.com>
2022-04-13 13:25:55 +0900

    ㅎㅅ

--- src/main/java/mna/homespital/HomespitalApplication.java
+++ src/main/java/mna/homespital/HomespitalApplication.java
@@ -3,6 +3,8 @@ package mna.homespital;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 @ServletComponentScan
 @SpringBootApplication
@@ -12,4 +14,9 @@ public class HomespitalApplication {
         SpringApplication.run(HomespitalApplication.class, args);
     }
 
+    // 소연 : ㅎㅅ ServerEndpointExporter 추가
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
 }
